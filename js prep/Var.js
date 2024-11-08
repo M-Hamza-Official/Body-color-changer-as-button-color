6fa859a60ec264c4c06d193fb5e059500c8599fb
muhammad hamza <hamzaofficial102@gmail.com>
2024-11-08 17:45:33 +0500

    Add my local file

---
+++ js prep/Var.js
@@ -0,0 +1,38 @@
+// console.log(b);
+
+// const b  =  "hello";
+
+// let s = [12,13,14,15,16];
+
+// let  x= [... s] ;
+
+// x.unshift(9)
+
+// let ans = 89;
+// let msg;
+// msg = ans>=18 ? "you are adult":"you are not";
+
+// console.log(msg);
+
+// let sum = (num1 , num2)=>{
+    
+//     let add = num1 + num2;
+    
+//     return add;
+    
+// }
+// console.log(sum(3,4));
+
+// for(var b = 7; b <= 20; b++){
+//     console.log(b);
+    
+// }
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+arr.forEach(e => {
+    console.log(e);
+    
+});
+
+
